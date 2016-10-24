1ef73f3a65b38069b3d03c24a6cb43a9bd85742d
Kazuhiko Kamitani <kazu@Kazuhiko-no-MacBook-Air.local>
2016-10-24 22:40:18 +0900

    seedファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "csv"
+
+companies_csv = CSV.readlines("db/images.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Image.create(name: row[1], url: row[2], map: row[3])
+end
+
+companies_csv = CSV.readlines("db/products.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Product.create(name: row[1], url: row[2], map: row[3])
+end
+
+companies_csv = CSV.readlines("db/reviews.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Review.create(name: row[1], url: row[2], map: row[3])
+end
+
+companies_csv = CSV.readlines("db/users.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  User.create(name: row[1], url: row[2], map: row[3])
+end
